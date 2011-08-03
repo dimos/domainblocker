e4ce2e0ebc9a96800ef419652e60434fe98b9e04
Dimos Poupos <dimospoupos@gmail.com>
2011-08-03 15:42:01 +0300

    transporter.py file updated

--- transporter.py
+++ transporter.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+#       transporter.py
+#       
+#       Copyright 2011 Dimos Poupos <dimosrc@gmail.com>
+#       
+#       This program is free software; you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation; either version 2 of the License, or
+#       (at your option) any later version.
+#       
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#       
+#       You should have received a copy of the GNU General Public License
+#       along with this program; if not, see <http://www.gnu.org/licenses/>
+#		or write to the Free Software Foundation, Inc., 51 Franklin Street,
+#		Fifth Floor, Boston, MA 02110-1301, USA.
+#       
+#       
 
 import os
 
@@ -9,6 +31,9 @@ def cp_progress(fdest):
 	cp_footer = fdest
 	cpcmd = cp_header + cp_footer
 	os.system(cpcmd)
+	os.system("clear")
+	print("[Η διαδικασία ολοκληρώθηκε!]")
+	print("Για να τρέξετε το πρόγραμμα δώστε: sudo ",fdest)
 
 def main():
 	os.system("clear")
